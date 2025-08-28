87a80077aa055b5d609b262b6bd3520dbaffc5dd
Bob <r.bonde@utah.edu>
2025-08-27 18:01:28 -0600

    Initializing the repository

---
+++ 2026 ChemECar Arduino Sketches/sketch_aug27a.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // Wait for button/switch to turn on Arduino
+  //Actuate valve or compress hose to START gas flow
+
+
+
+}
+
+void loop() {
+  // Check stopping reaction 
+  // Actuate valve or compress hose to STOP gas flow
+
+}
