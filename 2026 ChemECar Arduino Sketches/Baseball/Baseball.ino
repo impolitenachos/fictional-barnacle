e5a92a9536c0851004b65d94d76328c02a08c169
impolitenachos <r.bonde@utah.edu>
2025-09-17 19:11:20 -0600

    Adjusted sensor threshold and added TODOs, please add plotting feature.

--- 2026 ChemECar Arduino Sketches/Baseball/Baseball.ino
+++ 2026 ChemECar Arduino Sketches/Baseball/Baseball.ino
@@ -4,42 +4,52 @@ bool carRunning;
 bool restartCar = true;  //For debugging threshold
 int count;
 int lightSensor;
+bool debugMotor = true;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
   pinMode(A0, INPUT);  // Pin A0
   carRunning = true;
-  thresholdVoltage = 2.5;
-  threshold = thresholdVoltage * 1023 / 5;  // Turning a threshold voltage into a value understood by Arduino
+  thresholdVoltage = 2.0;
+  threshold = thresholdVoltage * 1023 / 3.3;  // Turning a threshold voltage into a value understood by Arduino
   count = 0;
   Serial.begin(9600);
   delay(1000);
+  //TODO: 
+  /* Add blinking LED to show it's ready
+  * Maybe turn open valve?? 
+  */
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly 
   lightSensor = analogRead(A0);
-  if (carRunning) {
+  if (carRunning && lightSensor < threshold) {
+    //TODO: add solid LED code 
     digitalWrite(13, HIGH);
     delay(200);
     Serial.print("Light ");
-    Serial.print(float(lightSensor) * 5 / 1023);
+    Serial.print(float(lightSensor) * 3.3 / 1023);
     Serial.println("V");
+    Serial.println(millis()/1000.0);
   } else
     digitalWrite(13, LOW);
 
-  if (lightSensor < threshold) {
+  if (lightSensor > threshold) {
+    if(!debugMotor)
     count++; 
     if (count > 4 ) {
     carRunning = false;
     Serial.println("Car stopped");
+    // TODO: turn off LED
     }
     Serial.print("Dark ");
-    Serial.print(float(lightSensor) * 5 / 1023);
+    Serial.print(float(lightSensor) * 3.3 / 1023);
     Serial.print("V. ");
     Serial.print(count);
     Serial.println(" times. ");
+    Serial.println(millis()/1000.0);
     delay(200);
   } else {
     if (restartCar) {
